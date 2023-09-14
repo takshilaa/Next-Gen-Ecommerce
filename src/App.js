03f9ee4a96bd0dbac4339b819a5b243f54bbfe6d
takshilaa <takshila08@gmail.com>
2023-09-14 10:52:44 +0530

    Cart and Product Detail Page is added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
 } from 'react-router-dom';
 import Cart from './features/cart/Cart';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 const router = createBrowserRouter([
   {
@@ -29,6 +31,14 @@ const router = createBrowserRouter([
   { // only for testing - then page will be added
     path: '/cart',
     element: <CartPage></CartPage>,
+  },
+  { 
+    path: '/checkout',
+    element: <Checkout></Checkout>,
+  },
+  { 
+    path: '/product-detail',
+    element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
 
